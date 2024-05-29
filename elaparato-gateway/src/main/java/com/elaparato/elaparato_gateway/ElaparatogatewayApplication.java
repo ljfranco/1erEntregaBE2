6d49116d8769c901f78d5cda5c02fbc4dc5d64ce
Leonardo <leonardojfs83@gmail.com>
2024-05-28 23:32:57 -0300

    elaparato-gateway added

---
+++ elaparato-gateway/src/main/java/com/elaparato/elaparato_gateway/ElaparatogatewayApplication.java
@@ -0,0 +1,13 @@
+package com.elaparato.elaparato_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElaparatogatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElaparatogatewayApplication.class, args);
+	}
+
+}
